6e0bb938993ba879771e3a8abb16562c29ebdd16
Wassup <76683390+zlx454545@users.noreply.github.com>
2023-06-16 10:50:43 +0900

    Revert "FEAT: TabBar를 이용한 라우터 기능 구현"
    This reverts commit a1bdf940ac412544a14a3ec3ffc924439efa7111.

--- frontend/src/components/App/index.tsx
+++ frontend/src/components/App/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components';
 
@@ -10,28 +10,23 @@ import HeartPage from '../HeartPage';
 import ChatPage from '../ChatPage';
 import AccountPage from '../AccountPage';
 import ErrorPage from '../ErrorPage';
-import Layout from '../Layout';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: '/', element: <HomePage /> },
-      { path: '/sales', element: <SalesPage /> },
-      { path: '/heart', element: <HeartPage /> },
-      { path: '/chat', element: <ChatPage /> },
-      { path: '/account', element: <AccountPage /> },
-    ],
-  },
-]);
+import TabBar from '../TabBar';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sales" element={<SalesPage />} />
+          <Route path="/heart" element={<HeartPage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
+      <TabBar />
     </ThemeProvider>
   );
 };
--- frontend/src/components/TabBar/index.tsx
+++ frontend/src/components/TabBar/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 import iconfiles from '../../assets/icons/index';
@@ -11,15 +10,14 @@ interface Tab {
   id: number;
   name: IconName;
   label: string;
-  path: string;
 }
 
 const TABS: Tab[] = [
-  { id: 0, name: 'home', label: '홈화면', path: '/' },
-  { id: 1, name: 'newspaper', label: '판매내역', path: '/sales' },
-  { id: 2, name: 'heart', label: '관심목록', path: '/heart' },
-  { id: 3, name: 'message', label: '채팅', path: '/chat' },
-  { id: 4, name: 'person', label: '내 계정', path: '/account' },
+  { id: 0, name: 'home', label: '홈화면' },
+  { id: 1, name: 'newspaper', label: '판매내역' },
+  { id: 2, name: 'heart', label: '관심목록' },
+  { id: 3, name: 'message', label: '채팅' },
+  { id: 4, name: 'person', label: '내 계정' },
 ];
 
 const TabBar = () => {
@@ -39,9 +37,7 @@ const TabBar = () => {
               active={activeTab === tab.id}
               onClick={() => handleClick(tab.id)}
             >
-              <Link to={tab.path}>
-                <Icon name={tab.name} width={'48'} height={'26'} />
-              </Link>
+              <Icon name={tab.name} width={'48'} height={'26'} />
               <span>{tab.label}</span>
             </S.Li>
           ))}
