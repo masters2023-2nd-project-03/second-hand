093c9e7b64981a70347e13263c2ec355e6a87224
Wassup <76683390+zlx454545@users.noreply.github.com>
2023-06-16 10:50:43 +0900

    Revert "FEAT: elapsedTime 유틸 생성 (#54)"
    This reverts commit bb6c0ddd1a3c736f82640cf73a8139d38ab9f802.

--- frontend/src/components/SecondHandItem/index.tsx
+++ frontend/src/components/SecondHandItem/index.tsx
@@ -1,8 +1,5 @@
-import Button from '../Button';
-import Icon from '../Icon';
-import elapsedTime from '../../utils/elapsedTime';
-
 interface SecondHandItemProps {
+  id: number;
   title: string;
   createdAt: string;
   status: string;
@@ -11,10 +8,10 @@ interface SecondHandItemProps {
   chatCount: number;
   interestCount: number;
   imageURI: string;
-  option?: boolean;
 }
 
 const SecondHandItem = ({
+  id,
   title,
   createdAt,
   status,
@@ -23,40 +20,11 @@ const SecondHandItem = ({
   chatCount,
   interestCount,
   imageURI,
-  option = false,
 }: SecondHandItemProps) => {
   return (
-    <div className="ItemContainer">
-      <div
-        className="ItemImage"
-        style={{
-          width: '120px',
-          height: '120px',
-          backgroundImage: `url(${imageURI})`,
-          backgroundSize: 'contain',
-        }}
-      />
-      <div className="ItemInfo">
-        <div className="ItemContents">
-          <div className="Content">
-            <div className="ColumnTop">{title}</div>
-            <div className="ColumnMiddle">
-              {location} ・ {elapsedTime(createdAt)}
-            </div>
-            <div className="ColumnBottom">
-              <Button round={true}>{status}</Button>
-              <span>{price}원</span>
-            </div>
-          </div>
-          <div className="Option">
-            {option && <Icon name={'ellipsis'} width={'17'} height={'20'} />}
-          </div>
-        </div>
-        <div className="ItemIssue">
-          {chatCount}
-          {interestCount}
-        </div>
-      </div>
+    <div>
+      {id}, {title}, {createdAt}, {status}, {price}, {location},{chatCount},{' '}
+      {interestCount},{imageURI}
     </div>
   );
 };
