c0a3d2341165b319e5f984ea8e452174d17c1edb
sarang_daddy <109648042+sarangdaddy@users.noreply.github.com>
2023-06-28 02:03:18 +0900

    Revert "FEAT: 채팅 input 컴포넌트 UI 생성 (#117)"
    This reverts commit 1c6e61215fd4afed6f42a04fab368e5e943e0a94.

--- frontend/src/pages/ChatRoom/index.tsx
+++ frontend/src/pages/ChatRoom/index.tsx
@@ -1,7 +1,6 @@
-import ChatRoomContents from '../../components/ChatRoomContents';
 import ChatRoomItem from '../../components/ChatRoomItem';
-import ChatInputBar from '../../components/ChatInputBar';
 import NavBarTitle from '../../components/NavBarTitle';
+import * as S from './styles';
 
 const ChatRoom = () => {
   return (
@@ -13,9 +12,11 @@ const ChatRoom = () => {
         centerTitle="판매자"
         moreIcon
       />
-      <ChatRoomItem />
-      <ChatRoomContents />
-      <ChatInputBar />
+      <S.Main>
+        <ChatRoomItem />
+        <div className="ChatContents">채팅내용</div>
+      </S.Main>
+      <div className="ChatInputBar">입력</div>
     </>
   );
 };
