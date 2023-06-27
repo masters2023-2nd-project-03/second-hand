8b39fa321af40ed190985d44eafdb4a9e78af142
sarang_daddy <109648042+sarangdaddy@users.noreply.github.com>
2023-06-28 02:03:18 +0900

    Revert "FEAT: DetailTapBar 컴포넌트 구현 (#114)"
    This reverts commit 8afbea8584e26b733ddd9e2bffef57d7825e7bfa.

--- frontend/src/pages/ItemDetail/index.tsx
+++ frontend/src/pages/ItemDetail/index.tsx
@@ -3,7 +3,6 @@ import { useParams, useNavigate } from 'react-router-dom';
 import NavBarTitle from '../../components/NavBarTitle';
 import DetailSliderPhotos from '../../components/DetailSliderPhotos';
 import DetailItem from '../../components/DetailItem';
-import DetailTapBar from '../../components/DetailTapBar';
 import * as S from './styles';
 
 interface Item {
@@ -66,7 +65,6 @@ const ItemDetail = () => {
         option={false}
       />
       <S.Main>
-        <div>현재 제품은 {productsId} 번 입니다.</div>
         <div className="DetaulSellerInfo">판매자 정보</div>
         <DetailItem
           title={sampleItem.title}
@@ -79,8 +77,9 @@ const ItemDetail = () => {
           isWatchlistChecked={sampleItem.isWatchlistChecked}
           productMainImgUrl={sampleItem.productMainImgUrl}
         />
+        <div className="DetailTapBar">관심 + 채팅</div>
       </S.Main>
-      <DetailTapBar price={sampleItem.price} />
+      <div>현재 제품은 {productsId} 번 입니다.</div>
     </>
   );
 };
